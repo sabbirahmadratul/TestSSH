6de79f9c75bff6cb8f7fd452e363d93480958ab8
Sabbir Ahmad Ratul <sabbirahmadratul@gmail.com>
2023-03-30 13:00:46 +0600

    Second

--- Demo.WebAPI/Models/UpdateBrand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Demo.WebAPI.Models
-{
-  public class UpdateBrand
-  {
-    public string? Name { get; set; }
-    public string? Category { get; set; }
-    public string? Model { get; set; }
-  }
-}
